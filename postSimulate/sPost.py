69f7436642babf97e0c5d2a2f2e72a8926e3e8eb
wangzy48698 <wangzy48698@hundsun.com>
2023-08-09 16:37:57 +0800

    export csv

--- postSimulate/sPost.py
+++ postSimulate/sPost.py
@@ -1,10 +1,12 @@
+import os
 import requests
 import time
 import json
+import csv
+import asyncio
 
 
-def send(phone):
-    print('token:', token)
+async def getUser(phone):
     ts = int(round(time.time() * 1000))
     url = 'https://admin.edujia.com/admin/wmim/user/qryList.do?ts=%d' % ts
     headers = {
@@ -28,19 +30,52 @@ def send(phone):
     print('data:', json.dumps(data))
     try:
         ret = requests.post(url=url, headers=headers, data=json.dumps(data))
+        print('ret:', ret)
         response_data = ret.json()
         print('response_data:', response_data)
+        if response_data.get('code') == '200':
+            rlist = response_data.get('data').get('array')
+            print('rlist:', rlist)
+            return rlist
+        else:
+            return []
     except Exception as e:
-        print('send e:', e)
+        print('getUser e:', e)
+        return []
+
+
+def trans(dict):
+    keys = ['phone', 'nickname', 'wxNickname', 'registerIp', 'createTime']
+    res = []
+    for key in keys:
+        res.push(dict[key])
+    return res
+
+
+async def search():
+    # 读取txt中电话号，逐行处理
+    with open('phones.txt', 'rb') as phonesFile:
+        for pn in phonesFile.readlines():
+            phone = pn.strip().decode()
+            print('phone:', phone)
+            # 发送请求查询相关用户
+            userList = await getUser(phone)
+            # 以电话号为名创建csv文件
+            fileName = '%s.csv' % phone
+            isExists = os.path.exists(fileName)
+            with open(fileName, mode='w' if not isExists else 'a', newline='') as file:
+                writer = csv.writer(file)
+                if (not isExists):
+                    writer.writerow(['phone', 'nickname', 'wxNickname', 'registerIp', 'createTime'])
+                for user in userList:
+                    writer.writerow(trans(user))
+            input('电话%s查询完成，发现%d个用户，回车继续' % (phone, len(userList)))
 
 
 if __name__ == '__main__':
     global token, userid
     userid = input('输入Userid (回车直接跳过, 使用默认A-fa7a5900): ')
     token = input('先登录系统, 获取token后粘贴到这里后敲回车: ')
-
-    with open('phones.txt', 'rb') as phonesFile:
-        for pn in phonesFile.readlines():
-            print('pn:', pn.strip().decode())
-            phone = pn.strip().decode()
-            send(phone)
+    print('userid:', 'A-fa7a5900' if userid == '' else userid)
+    print('token:', token)
+    asyncio.run(search())
